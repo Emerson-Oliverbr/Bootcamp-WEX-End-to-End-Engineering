f8209ed68ce5e16d742c0043da97deed35fe1055
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-05-30 16:41:35 -0300

    Finalizando o conceito de classes

--- Exemplo Fundamantos/SintaxeBasica/Program.cs
+++ Exemplo Fundamantos/SintaxeBasica/Program.cs
@@ -1,10 +1,16 @@
 ﻿using SintaxeBasica.Models;
 
 Pessoa pessoa1 = new Pessoa();
+Pessoa pessoa2 = new Pessoa();
 
 pessoa1.Nome = "Emerson Oliveira";
-pessoa1.Idade = 26;
+pessoa1.Idade = 40;
 pessoa1.Apresentar();
+
+pessoa2.Nome = "Daniela Oliveira";
+pessoa2.Idade = 36;
+pessoa2.Apresentar();
+
 
 
 
