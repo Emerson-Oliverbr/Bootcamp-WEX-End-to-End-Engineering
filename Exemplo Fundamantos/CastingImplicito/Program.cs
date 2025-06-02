17d8f949e996a100ccb9c75d99011352f3966707
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-06-01 23:06:09 -0300

    Casting implicito

---
+++ Exemplo Fundamantos/CastingImplicito/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 5;
+double b = a; //Conversão implicita "Automatica'
+Console.WriteLine(b);
+
+double c = 5.1;
+//int d = c;
+
+Console.WriteLine(c);
+//Console.WriteLine(d); // Erro na saida conversão impossível de double para int
+
