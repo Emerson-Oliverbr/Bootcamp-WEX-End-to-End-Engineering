aad47335c988d4a9c9d9514ec0c0dbdfafd27a88
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-06-01 22:41:32 -0300

    Diferença entre Convert e Parse

--- Exemplo Fundamantos/TiposDeOperadores/Program.cs
+++ Exemplo Fundamantos/TiposDeOperadores/Program.cs
@@ -15,4 +15,19 @@ Console.WriteLine(C);
 C-= 5;
 Console.WriteLine(C);
 
+/*************************************************************************************************/
+/*Aprendendo Casting*/
+
+int a = Convert.ToInt32("5"); // "Convert" é uma classe "ToInt32" é um metodo que recebe uma string e converte em um inteiro 
+int b = int.Parse("10");
+double c = double.Parse("2.5");
+decimal d = decimal.Parse("5.75");     
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(c.ToString("F1"));
+Console.WriteLine(d.ToString("F2"));
+
 
