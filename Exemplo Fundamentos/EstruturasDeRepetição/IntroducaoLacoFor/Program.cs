0863c7df40226215c6c2d47acdb7295779196e13
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-06-03 20:14:03 -0300

    Estrututuras de repetição

---
+++ Exemplo Fundamentos/EstruturasDeRepetição/IntroducaoLacoFor/Program.cs
@@ -0,0 +1,7 @@
+﻿int numero = 10;
+
+for (int contador = 0; contador <= 10; contador++)
+{
+    
+    Console.WriteLine($"{numero} * {contador} = {numero * contador}");
+}
---
+++ Exemplo Fundamentos/EstruturasDeRepetição/IntroducaoLacoWhile/Program.cs
@@ -0,0 +1,13 @@
+﻿int numero = 10;
+int contador = 1;
+
+while (contador <= 10)
+{
+    Console.WriteLine($"{contador}ª Execução {numero} * {contador} = {numero * contador}");
+    contador += 1;
+
+    if (contador == 6)
+    {
+        break;
+    }    
+}
